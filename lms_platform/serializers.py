a1046d8254f8bb3ea007089c0022a4af0ba9b7b5
aleksandrveselov <sanek_ves@mail.ru>
2023-08-02 13:51:55 +0300

    задание 3

--- lms_platform/serializers.py
+++ lms_platform/serializers.py
@@ -4,23 +4,29 @@ from rest_framework.fields import SerializerMethodField
 from lms_platform.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Урок"""
+
+    class Meta:
+        model = Lesson  # Модель
+        fields = '__all__'  # Поля
+
+
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Курс"""
 
     count_lessons = SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    # lessons = SerializerMethodField()
 
     def get_count_lessons(self, course):
         """Метод для получения количества уроков в курсе"""
         return Lesson.objects.filter(course=course).count()
 
+    def get_lessons(self, course):
+        print([el.pk for el in Lesson.objects.filter(course=course)])
+        return [el.pk for el in Lesson.objects.filter(course=course)]
+
     class Meta:
         model = Course  # Модель
         fields = '__all__'  # ПОля
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Урок"""
-    class Meta:
-        model = Lesson  # Модель
-        fields = '__all__'  # Поля
-
